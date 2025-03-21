90d3175f2b16fb004e1e7d49ec09ccbf82ec2086
Souad Yakubu <sny2@calvin.edu>
2025-03-21 17:53:58 -0400

    hide keys in env

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} /> {/* ✅ Added Home Route */}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
--- my-app/src/services/firebase.js
+++ my-app/src/services/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1KjHJ94kApyNCj-GTlebIMXGmUvNW6K8",
-    authDomain: "cooking-3d97a.firebaseapp.com",
-    projectId: "cooking-3d97a",
-    storageBucket: "cooking-3d97a.firebasestorage.app",
-    messagingSenderId: "892633833123",
-    appId: "1:892633833123:web:2cbf07be14ed7a9742a916",
-    measurementId: "G-97BW6V738J"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
